bd5490af04fff6f215aac73a327ec2fdbdd9fb2d
mathemacode <43391344+mathemacode@users.noreply.github.com>
2020-04-21 16:36:56 -0400

    Fix dtypes

--- SQL/nursery_buildDB.sql
+++ SQL/nursery_buildDB.sql
@@ -9,8 +9,8 @@ DROP TABLE IF EXISTS NURSERY_ANALYSIS;
 
 CREATE TABLE FACILITY (
 	Facility_ID INTEGER PRIMARY KEY NOT NULL,
-	Facility_UIN INTEGER NOT NULL,
-	PermitNumber INTEGER,
+	Facility_UIN NUMERIC,
+	PermitNumber VARCHAR(15),
 	PermitType VARCHAR(6),
 	FacilityName VARCHAR(100),
 	FacilityType VARCHAR(50),
@@ -20,8 +20,8 @@ CREATE TABLE FACILITY (
 	Has_P_Limits VARCHAR(6),
 	Has_N_Monitoring VARCHAR(6),
 	Has_P_Monitoring VARCHAR(6),
-	SICCODE INTEGER,
-	NAICS_CODE INTEGER,
+	SICCODE VARCHAR(15),
+	NAICS_CODE VARCHAR(15),
 	Major_Minor_Flag CHAR(1),
 	Design_Flow NUMERIC,
 	Actual_Flow NUMERIC
@@ -37,9 +37,10 @@ CREATE TABLE CLIMATE (
 
 CREATE TABLE CHEMICAL (
 	Chemical_ID INTEGER PRIMARY KEY NOT NULL,
+	Facility_Ref INTEGER,
 	Chem_Name VARCHAR(50),
-	Min_Limit VARCHAR(6),
-	Max_Limit VARCHAR(6),
+	Min_Limit VARCHAR(20),
+	Max_Limit VARCHAR(20),
 	Units VARCHAR(10)
 );
 
@@ -47,14 +48,30 @@ CREATE TABLE LOCATION (
 	Location_ID INTEGER PRIMARY KEY NOT NULL,
 	City VARCHAR(50),
 	State VARCHAR(50),
-	Zip NUMERIC,
+	Zip VARCHAR(15),
 	County VARCHAR(50),
 	EPARegion VARCHAR(50),
 	Latitude NUMERIC,
 	Longtitude NUMERIC,
-	PrcntRiversImpaired NUMERIC,
+	State_Long VARCHAR(30),
+	AssessedLakesAcres NUMERIC,
+	PrcntAssessedLakes VARCHAR(25), 
+	LakesWithNutrientImpairAcres NUMERIC,
+	PrctLakesImpaired NUMERIC,
+	PrcntLakesImparedwithRestorationPlan NUMERIC,
+	LakesReportYear NUMERIC, 
+	AssessedRiversMi NUMERIC, 
+	PrcntAssessedRivers VARCHAR(25),
+	RiversWithNutrientImpairMiles NUMERIC, 
+	PrctRiversImpaired NUMERIC,
+	PrcntRiversImparedwithRestorationPlan NUMERIC, 
+	RiversReportYear NUMERIC,
+	AssessedEstuariesSqMi NUMERIC, 
+	PrcntAssessedEstuaries VARCHAR(25),
+	EstuariesWithNutrientImpairSqMi NUMERIC, 
 	PrcntEstuariesImpaired NUMERIC,
-	PrcntLakesImpaired NUMERIC
+	PrcntEstuariesImparedwithRestorationPlan NUMERIC, 
+	EstuariesReportYear NUMERIC
 );
 
 CREATE TABLE DATE (
@@ -78,3 +95,11 @@ CREATE TABLE NURSERY_ANALYSIS (
 	NumAbnormalHi NUMERIC,
 	NumAbnormalLo NUMERIC
 );
+
+COPY FACILITY FROM 'C:/Users/dell/Documents/GitHub/NurseryWarehouse/data/transformed_data/FACILITY.csv' (FORMAT CSV, DELIMITER(','), HEADER);
+--COPY CLIMATE FROM 'C:/Users/dell/Documents/GitHub/NurseryWarehouse/data/transformed_data/CLIMATE.csv' (FORMAT CSV, DELIMITER(','), HEADER);
+COPY CHEMICAL FROM 'C:/Users/dell/Documents/GitHub/NurseryWarehouse/data/transformed_data/CHEMICAL.csv' (FORMAT CSV, DELIMITER(','), HEADER);
+COPY LOCATION FROM 'C:/Users/dell/Documents/GitHub/NurseryWarehouse/data/transformed_data/LOCATION.csv' (FORMAT CSV, DELIMITER(','), HEADER);
+--COPY DATE FROM 'C:/Users/dell/Documents/GitHub/NurseryWarehouse/data/transformed_data/DATE.csv' (FORMAT CSV, DELIMITER(','), HEADER);
+--COPY NURSERY_ANALYSIS FROM 'C:/Users/dell/Documents/GitHub/NurseryWarehouse/data/transformed_data/NURSERY_ANALYSIS.csv' (FORMAT CSV, DELIMITER(','), HEADER);
+
