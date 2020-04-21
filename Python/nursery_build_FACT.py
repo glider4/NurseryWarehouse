efcad312b98a742a0cc9df5cbdbbc393796b8774
mathemacode <43391344+mathemacode@users.noreply.github.com>
2020-04-21 17:21:01 -0400

    Begin building fact table NURSERY_ANALYSIS

---
+++ Python/nursery_build_FACT.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Build main FACT table (NURSERY_ANALYSIS) based on dimension tables already
+built in the nursery_transform script.
+"""
+
+import pandas as pd
+
+# Set paths to transformed/cleaned data files
+facilities_path = 'C:/Users/dell/Documents/GitHub/NurseryWarehouse/data/transformed_data/FACILITY.csv'
+climate_path = 'C:/Users/dell/Documents/GitHub/NurseryWarehouse/data/transformed_data/CLIMATE.csv'
+location_path = 'C:/Users/dell/Documents/GitHub/NurseryWarehouse/data/transformed_data/LOCATION.csv'
+chemical_path = 'C:/Users/dell/Documents/GitHub/NurseryWarehouse/data/transformed_data/CHEMICAL.csv'
+#date_path = 'C:/Users/dell/Documents/GitHub/NurseryWarehouse/data/transformed_data/DATE.csv'
+
+# Import clean data files
+facilities = pd.read_csv(facilities_path, delimiter=',')
+climate = pd.read_csv(climate_path, delimiter=',')
+location = pd.read_csv(location_path, delimiter=',')
+chemical = pd.read_csv(chemical_path, delimiter=',')
+
